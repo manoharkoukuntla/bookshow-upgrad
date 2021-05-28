3060c932bb3e5bf48ee9f42d007375539448105e
manohark <manohark@spiceworks.com>
2021-05-28 12:46:54 +0530

    add upcoming movies component

--- src/screens/movies/Movies.js
+++ src/screens/movies/Movies.js
@@ -12,23 +12,35 @@ const styles = (theme) => ({
     marginLeft: 'auto',
     marginRight: '30px',
   },
+  gridListTile: {
+    cursor: 'pointer',
+  },
+  upcomingMoviesComponent: {
+    overflow: 'scroll',
+    width: '100%',
+  },
   singleLineGridList: {
-    flex: 'nowrap',
+    flexWrap: 'nowrap',
   },
 });
+
+const pageLimit = 10;
 
 function Movies(props) {
   const { classes } = props;
   const [movies, setMovies] = useState([]);
+  const [releasedMoviesPages, setReleasedMoviesPages] = useState(1);
+  const [upComingMoviesPages, setUpComingMoviesPages] = useState(1);
   const [upComingMovies, setUpComingMovies] = useState([]);
-  const tomorrow = moment().add(1, 'day').format('yyyy-MM-DD');
 
   useEffect(() => {
     (async () => {
       const moviesResponse = await getMovies({ status: 'RELEASED' });
-      const upComingMoviesResponse = await getMovies({ page: 1, limit: 6, startDate: tomorrow });
+      const upComingMoviesResponse = await getMovies({ status: 'PUBLISHED' });
 
       setMovies(moviesResponse.movies);
+      setReleasedMoviesPages(Math.ceil(moviesResponse.totalCount / pageLimit));
+      setUpComingMoviesPages(Math.ceil(upComingMoviesResponse.totalCount / pageLimit));
       setUpComingMovies(upComingMoviesResponse.movies);
     })();
   }, []);
@@ -42,7 +54,7 @@ function Movies(props) {
       style={{ margin: '24px auto 24px 32px' }}
     >
       {movies.map((movie) => (
-        <GridListTile key={movie.id}>
+        <GridListTile key={movie.id} className={classes.gridListTile}>
           <img src={movie.posterUrl} alt={movie.title} />
           <GridListTileBar
             title={movie.title}
@@ -56,7 +68,7 @@ function Movies(props) {
   );
 
   const upcomingMoviesComponent = (
-    <GridList className={classes.singleLineGridList} cols={5}>
+    <GridList className={classes.singleLineGridList} cols={6}>
       {upComingMovies.map((movie) => (
         <GridListTile key={movie.id}>
           <img src={movie.posterUrl} alt={movie.title} />
@@ -69,7 +81,7 @@ function Movies(props) {
     <div>
       <Header baseUrl={props.baseUrl} />
       <div className="upComingMovies">Upcoming Movies</div>
-      {upcomingMoviesComponent}
+      <div className={classes.upcomingMoviesComponent}>{upcomingMoviesComponent}</div>
       <div className="flex">
         <div className="moviesList">{moviesComponent}</div>
         <div className="moviesFilter">Filters</div>
